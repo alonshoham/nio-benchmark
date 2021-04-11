ac17330b86f5f54d116e4ffb59dc53c2fef31683
nivin <niv@gigaspaces.com>
2021-04-11 12:56:52 +0300

    fixed benchmark main class

--- nio-poc-benchmark/batch-client.sh
+++ nio-poc-benchmark/batch-client.sh
@@ -8,7 +8,7 @@ do
         for t in 1 2 4 8 16 32 64
         do
             echo "*** Starting benchmark with $c and $w workers for $t benchmark threads"
-            java -cp target/nio-poc-benchmark-0.1-jar-with-dependencies.jar org.openjdk.jmh.Main nio.JMHClientMain -rff $c-w$w-t$t.csv -f 1 -i 5 -t $t
+            java -cp target/nio-poc-benchmark-0.1-jar-with-dependencies.jar org.openjdk.jmh.Main jmh.benchmarks.basic.ReadByIdBenchmark -rff $c-w$w-t$t.csv -f 1 -i 5 -t $t
         done
     done
 done
